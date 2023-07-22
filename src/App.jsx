79a261bd7ff660a1b3c94ad054c29cd2352b26f0
nachodev-ui <ig.cisternas@duocuc.cl>
2023-07-22 04:55:20 -0400

    feat: Main component where we rendering WeatherProvider & WeatherApp second main comp

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { WeatherApp } from "./components/WeatherApp"
+import { WeatherProvider } from "./context/WeatherProvider"
+
+function App() {
+  return (
+    <WeatherProvider>
+      <header>
+        <h1>Weather App</h1>
+      </header>
+
+      <WeatherApp />
+    </WeatherProvider>
+  )
+}
+
+export default App
